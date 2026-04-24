8571af8eb1efe4dac8d227f447979c8d76b05640
Mirthulaa04 <vairavel9790@gmail.com>
2026-04-24 18:34:40 +0530

    Add files via upload

--- Lab Task 6.py
+++ Lab Task 6.py
@@ -1,17 +1,17 @@
 '''
-#1)chect whether a number is even or odd
+#1)Chect whether a number is even or odd
 s=int(input("Enter your no:"))
 def num(s):
     if s%2==0:
-        print("even no")
+        print("Even no")
     else:
-        print("odd no")
+        print("Odd no")
 num(s)
 '''
 '''
-#2)find the maximum of two numbers
-a=int(input("enter your first no:"))
-b=int(input("enter your second no:"))
+#2)Find the maximum of two numbers
+a=int(input("Enter your first no:"))
+b=int(input("Enter your second no:"))
 def no(a,b):
     if a>b:
         print("a is max")
@@ -20,7 +20,7 @@ def no(a,b):
 no(a,b)
 '''
 '''
-#3)calculate the factorial number
+#3)Calculate the factorial number
 n=int(input("Enter your number"))
 def fac(n):
     m=1
@@ -30,41 +30,33 @@ def fac(n):
 fac(n)
 '''
 '''
-#4)sum of element in a list
-
-for i in range(1,10):
-    def num(i):
-        sum=0
+#4)Sum of element in a list
+def sum():
+    sum=0
+    for i in range(11):
         sum+=i
-print("sum=",sum)
-num(i)
+    print("the sum=",sum)
+sum()
 '''
-
 '''
-#6)reverse string
-n=("harini suresh")
+#6)Reverse string
+n=("Malayalam")
 def reverse(n):
     print(n[::-1])
 reverse(n)
 '''
 '''
-
-#7)check whether a number is prime
-n=int(input("enter your no:"))
+#7)Check whether a number is prime
+n=int(input("Enter your no:"))
 def prime(n):
-    if n<0:
-        print("not a prime no")
-    
-    else:
-        for i in range(2,n):
-            if n%i==0:
-                print("not a prime no")
-                break
-            else:
-                print("prime no")
+    for i in range(2,n):
+        if n%i==0:
+            print("Not a prime no")
+            break
+        else:
+            print("Prime no")
 prime(n)
 '''
-
 
       
 
